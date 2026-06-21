f5d3c1e86699e360186aa4b22ce6b39566d15300
Francisco Pereira <franciscoreisp@gmail.com>
2026-06-21 14:43:42 +0100

    Add files via upload

---
+++ Models/EmailModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trabalho.Models
+{
+    public class EmailModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Para")]
+        public string Para { get; set; }
+
+        [Required]
+        [Display(Name = "Assunto")]
+        public string Assunto { get; set; }
+
+        [Required]
+        [Display(Name = "Mensagem")]
+        public string Mensagem { get; set; }
+    }
+}
